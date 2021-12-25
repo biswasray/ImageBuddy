caa9e69cc904d6c0a520348c26e1451beadf6eb7
Subhasish Biswasray <subhasish9583672357@gmail.com>
2021-12-25 14:03:15 +0530

    Add resizing feature

--- ImageBuddy/Form1.Designer.cs
+++ ImageBuddy/Form1.Designer.cs
@@ -30,18 +30,29 @@ namespace ImageBuddy
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(48, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(78, 19);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(240, 240);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -49,55 +60,139 @@ namespace ImageBuddy
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // panel1
+            // label3
             // 
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(13, 13);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(352, 434);
-            this.panel1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 267);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Name : ???";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 353);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Resolution : ??? x ???";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 291);
+            this.label2.Location = new System.Drawing.Point(75, 324);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Size : ???";
             // 
-            // label3
+            // label1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(45, 315);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(113, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Resolution : ??? x ???";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name : ???";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(408, 434);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image details";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Location = new System.Drawing.Point(555, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(393, 434);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Edit";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.numericUpDown2);
+            this.groupBox3.Controls.Add(this.numericUpDown1);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(36, 44);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(330, 92);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Resize";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Width : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Height :";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(68, 26);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            -727379969,
+            232,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(68, 53);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            -727379969,
+            232,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Resize";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 461);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -105,10 +200,17 @@ namespace ImageBuddy
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
--- ImageBuddy/Form1.cs
+++ ImageBuddy/Form1.cs
@@ -14,11 +14,37 @@ namespace ImageBuddy
     public partial class Form1 : Form
     {
         private Bitmap myImage;
+        private Stream stream;
         public Form1()
         {
             InitializeComponent();
             pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
             pictureBox1.Image = pictureBox1.InitialImage;
+        }
+
+        private void LoadImage(Bitmap bitmap)
+        {
+            if (bitmap == null)
+                MessageBox.Show("Choose an Image");
+            try
+            {
+                myImage = bitmap;
+                stream = (new StreamWriter("tempImageBuudy00.png")).BaseStream;
+                myImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                FileInfo fileInfo = new FileInfo("tempImageBuudy00.png");
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.Image = myImage;
+                label2.Text = "Size : " + (stream.Length / 1024) + " KB ( " + stream.Length + " bytes )";
+                label3.Text = "Resolution : " + myImage.Width.ToString() + " x " + myImage.Height.ToString();
+                numericUpDown1.Value = myImage.Width;
+                numericUpDown2.Value = myImage.Height;
+                //myImage.Dispose();
+                stream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -28,14 +54,60 @@ namespace ImageBuddy
             open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
             if (open.ShowDialog() == DialogResult.OK)
             {
-                myImage = new Bitmap(open.FileName);
-                FileInfo fileInfo = new FileInfo(open.FileName);
-                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-                pictureBox1.Image = myImage;
-                label1.Text = "Name : "+open.FileName;
-                label2.Text = "Size : " + (fileInfo.Length/1024)+ " KB ( "+ fileInfo.Length+" bytes )";
-                label3.Text = "Resolution : " + myImage.Width.ToString()+" x "+ myImage.Height.ToString();
+                Bitmap b = new Bitmap(open.FileName);
+                LoadImage(b);
+                label1.Text = "Name : " + open.FileName;
+                //b.Dispose();
             }
+        }
+        public Bitmap ResizeImage(Bitmap bitmap,int newWidth,int newHeight)
+        {
+            if (newWidth == 0 || newHeight == 0)
+                return null;
+            Bitmap temp = new Bitmap(newWidth, newHeight, bitmap.PixelFormat);
+            double nWF = (double)bitmap.Width / (double)newWidth;
+            double nHF = (double)bitmap.Height / (double)newHeight;
+            double fx, fy, nx, ny;
+            int cx, cy, fr_x, fr_y;
+            Color color1 = new Color();
+            Color color2 = new Color();
+            Color color3 = new Color();
+            Color color4 = new Color();
+            byte nRed, nGreen, nBlue,bp1,bp2;
+            for(int x=0;x<temp.Width;++x)
+            {
+                for(int y=0;y<temp.Height;++y)
+                {
+                    fr_x = (int)Math.Floor(x * nWF);
+                    fr_y = (int)Math.Floor(y * nHF);
+                    cx = (fr_x + 1) >= bitmap.Width ? fr_x: (fr_x + 1);
+                    cy = (fr_y + 1) >= bitmap.Height ? fr_y : (fr_y + 1);
+                    fx = x * nWF - fr_x;
+                    fy = y * nHF - fr_y;
+                    nx = 1.0 - fx;
+                    ny = 1.0 - fy;
+                    color1 = bitmap.GetPixel(fr_x, fr_y);
+                    color2 = bitmap.GetPixel(cx, fr_y);
+                    color3 = bitmap.GetPixel(fr_x, cy);
+                    color4 = bitmap.GetPixel(cx, cy);
+                    bp1 = (byte)(nx * color1.B + fx * color2.B);
+                    bp2 = (byte)(nx * color3.B + fx * color4.B);
+                    nBlue = (byte)(ny * (double)(bp1) + fy * (double)(bp2));
+                    bp1 = (byte)(nx * color1.G + fx * color2.G);
+                    bp2 = (byte)(nx * color3.G + fx * color4.G);
+                    nGreen = (byte)(ny * (double)(bp1) + fy * (double)(bp2));
+                    bp1 = (byte)(nx * color1.R + fx * color2.R);
+                    bp2 = (byte)(nx * color3.R + fx * color4.R);
+                    nRed = (byte)(ny * (double)(bp1) + fy * (double)(bp2));
+                    temp.SetPixel(x, y, System.Drawing.Color.FromArgb(255, nRed, nGreen, nBlue));
+                }
+            }
+            return temp;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoadImage(ResizeImage(myImage, (int)numericUpDown1.Value, (int)numericUpDown2.Value));
         }
     }
 }
